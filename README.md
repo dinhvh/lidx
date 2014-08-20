ba02318a9383bb1e0299fef42a6f735ad70e7cec
Hoa V. Dinh <dinh.viet.hoa@gmail.com>
2014-08-20 16:31:39 -0700

    Add more documentation

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
 lidx
 ====
 
-Full text indexer based on LevelDB
+Unicode compatible full text indexer based on LevelDB.
+
+Requirements
+============
+- LevelDB
+- ICU library
+
+How to use it
+=============
+
+```
+lidx * indexer;
+int r;
+uint64_t * result;
+size_t result_count;
+
+indexer = lidx_new();
+r = lidx_open(indexer, "index.lidx");
+if (r < 0) {
+  // error handling.
+}
+
+r = lidx_set(indexer, 0, "George Washington");
+if (r < 0) {
+  // error handling.
+}
+
+r = lidx_set(indexer, 1, "John Adams");
+if (r < 0) {
+  // error handling.
+}
+
+r = lidx_set(indexer, 2, "Thomas Jefferson");
+if (r < 0) {
+  // error handling.
+}
+
+r = lidx_set(indexer, 3, "George Michael");
+if (r < 0) {
+  // error handling.
+}
+
+r = lidx_set(indexer, 4, "George Méliès");
+if (r < 0) {
+  // error handling.
+}
+
+print("searching geor");
+r = lidx_search(indexer, "geor", lidx_search_kind_suffix, &result, &result_count);
+for(size_t i = 0 ; i < result_count ; i ++) {
+  printf("found: %i\n", result[i]);
+}
+// returns 0, 3 and 4.
+free(result);
+
+print("searching mel");
+r = lidx_search(indexer, "mel", lidx_search_kind_suffix, &result, &result_count);
+for(size_t i = 0 ; i < result_count ; i ++) {
+  printf("found: %i\n", result[i]);
+}
+// return 4
+free(result);
+
+lidx_close(indexer);
+lidx_free(indexer);
+```
--- src/lidx.h
+++ src/lidx.h
@@ -9,6 +9,7 @@ extern "C" {
 #include <inttypes.h>
 #include <stdlib.h>
 
+// We're using the same UChar as the ICU library.
 #if defined(__CHAR16_TYPE__)
 typedef __CHAR16_TYPE__ UChar;
 #else
@@ -20,9 +21,9 @@ typedef struct lidx lidx;
 // prefix provides the best performance, two other options
 // have poor performance.
 typedef enum lidx_search_kind {
-  lidx_search_kind_prefix,
-  lidx_search_kind_substr,
-  lidx_search_kind_suffix,
+  lidx_search_kind_prefix, // Search documents that has strings that start with the given token.
+  lidx_search_kind_substr, // Search documents that has strings that contain the given token.
+  lidx_search_kind_suffix, // Search documents that has strings that end the given token.
 } lidx_search_kind;
 
 // Create a new indexer.
@@ -38,22 +39,33 @@ int lidx_open(lidx * index, const char * filename);
 void lidx_close(lidx * index);
 
 // Adds a UTF-8 document to the indexer.
+// `doc`: document identifier (numerical identifier in a 64-bits range)
+// `content`: content of the document in UTF-8 encoding.
 int lidx_set(lidx * index, uint64_t doc, const char * text);
 
 // Adds an unicode document to the indexer.
+// `content`: content of the document in UTF-16 encoding.
 int lidx_u_set(lidx * index, uint64_t doc, const UChar * utext);
 
-// Remove a document from the indexer.
+// Removes a document from the indexer.
 int lidx_remove(lidx * index, uint64_t doc);
 
-// Search a UTF-8 token in the indexer.
+// Searches a UTF-8 token in the indexer.
+// `token`: string to search in UTF-8 encoding.
+// `kind`: kind of matching to perform. See `lidx_search_kind`.
+// The result is an array of documents IDs. The array is stored in `*p_docsids`.
+// The number of items in the result array is stored in `*p_count`.
+//
+// The result array has to be freed using `free()`.
 int lidx_search(lidx * index, const char * token, lidx_search_kind kind,
     uint64_t ** p_docsids, size_t * p_count);
 
-// Search a unicode token in the indexer.
+// Searches a unicode token in the indexer.
+// `token`: string to search in UTF-16 encoding.
 int lidx_u_search(lidx * index, const UChar * utoken, lidx_search_kind kind,
     uint64_t ** p_docsids, size_t * p_count);
 
+// Writes changes to disk if they are still pending in memory.
 int lidx_flush(lidx * index);
 
 #ifdef __cplusplus
